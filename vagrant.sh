0b39675f6a118f251831d3bb33136bedee893bac
Thomas Omans <thomas.omans@gmail.com>
2014-03-04 14:49:47 -0800

    adding directory check to vagrant script

--- vagrant.sh
+++ vagrant.sh
@@ -3,9 +3,12 @@
 set -e
 
 # check that we have the required dependencies
-which vagrant ansible-playbook ansible-galaxy
+which vagrant ansible-playbook ansible-galaxy > /dev/null
 
 # install meatspace-chat into a vm, exposed on localhost:8080
-ansible-galaxy install -r ansible/roles.txt -p ansible/roles -f
+if [ ! -d ansible/roles/eggsby/supervise ]; then
+    ansible-galaxy install -r ansible/roles.txt -p ansible/roles -f
+fi
+
 vagrant up
 vagrant provision
